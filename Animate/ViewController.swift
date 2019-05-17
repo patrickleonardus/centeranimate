146d31b09d89e608eefc1da09a95f07d2e9d1a1d
Patrick Leonardus <patrickleonardus@gmail.com>
2019-05-17 15:59:59 +0700

    Initial Commit

--- Animate/ViewController.swift
+++ Animate/ViewController.swift
@@ -1,20 +1,39 @@
-//
-//  ViewController.swift
-//  Animate
-//
-//  Created by Patrick Leonardus on 17/05/19.
-//  Copyright © 2019 Patrick Leonardus. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myObject: UIButton!
+    @IBOutlet weak var myObj: UIView!
+    
+    var flag = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        myObject.layer.cornerRadius = myObject.frame.width/2
+    
+    
     }
-
+    
+    
+    @IBAction func btnObject(_ sender: Any) {
+        UIView.animate(withDuration: 1) {
+            if(self.flag==0){
+                self.myObject.center = CGPoint(x: 150, y: 175)
+                self.flag+=1
+            }
+            else if(self.flag==1){
+                self.myObject.center = self.view.center
+                self.flag-=1
+        
+            }
+            
+            
+        }
+        
+        
+    }
+    
 
 }
 
